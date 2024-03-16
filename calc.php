2242e893c52554ebe6feca20b004c5f1ba4c2ec4
ateves <39039918+ateves@users.noreply.github.com>
2024-03-15 20:25:47 -0400

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,14 @@
+<?php
+// Write your code below:
+function calculateArea($numA, $numB) {
+
+return $numA * $numB;
+}
+
+echo calculateArea(1, 5);
+
+function calculateVolume($newNumA, $newNumB, $newNumC) {
+return $newNumA * $newNumB * $newNumC;
+}
+
+echo calculateVolume(5, 10, 20);
